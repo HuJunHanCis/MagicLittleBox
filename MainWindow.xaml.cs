4f9f622ca03f846806f3f41b529564e3bedf2152
HuJunHanCis <z5353495@zmail.unsw.edu.au>
2025-11-24 14:21:15 +0800

    Organize MainWindow legacy backup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,49 +1,22 @@
-using System;
-using System.Diagnostics;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
-using Abb.Egm;
-using Newtonsoft.Json;
-
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
-using Abb.Egm;
-using Serilog;
-using System.IO;
-using System.Net;
-using System.Text;
-using System.Windows;
-using Google.Protobuf;
-using Newtonsoft.Json;
-using System.Threading;
-using System.Net.Sockets;
-using System.Runtime.Remoting.Messaging;
-using System.Windows.Media;
-using System.Windows.Input;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Text.RegularExpressions;
 
 namespace MagicLittleBox
 {
     public partial class MainWindow
     {
+
+        // Legacy code from version 0.1.2 is preserved in
+        // Legacy/MainWindow.Legacy_0.1.2.cs for reference during refactors.
         
         private void OnWindowDragMove(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
             {
                 DragMove();
-            }
-        }
+}
+}
+
         protected override void OnPreviewMouseDown(MouseButtonEventArgs e)
         {
             var element = FocusManager.GetFocusedElement(this);
